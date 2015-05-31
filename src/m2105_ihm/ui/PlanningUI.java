5408d21554875dd4893d378455f670f58ea334e1
Colas <colas@qwerty-PC>
2015-05-31 22:32:03 +0200

    Added : dynamic calendar (need to modify number of day in month)

--- src/m2105_ihm/ui/PlanningUI.java
+++ src/m2105_ihm/ui/PlanningUI.java
@@ -4,20 +4,14 @@
 package m2105_ihm.ui;
 
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-import javax.swing.BorderFactory;
-import javax.swing.DefaultListModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.tree.TreeSelectionModel;
 
 import m2105_ihm.Controleur;
 import m2105_ihm.nf.Evenement;
@@ -31,12 +25,13 @@ public class PlanningUI extends JPanel {
     /**
      * Creates new form CarnetUI
      */
-    private Controleur       controleur;
-    private FicheEvtUI       ficheEvt;
+    private Controleur controleur;
+    private FicheEvtUI ficheEvt;
     private JPanel nord;
     private JPanel centre;
     private JComboBox mois;
     private JComboBox annee;
+    private ArrayList<JLabel> jours;
 
     /** 
      * Constructeur : initialise les composants de l'IHM pour les événements
@@ -64,57 +59,43 @@ public class PlanningUI extends JPanel {
         nord.setLayout(new GridLayout(1, 2));
         
         mois = new JComboBox(Mois.values());
+        
+        mois.setSelectedItem(Mois.JUIN);
+        
+        mois.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                setCalendar();
+                centre.updateUI();
+            }
+        });
+        
         annee = new JComboBox();
         
-        for (int i = 1900; i < 2015; i++){
+        for (int i = 1900; i <= 2015; i++){
             annee.addItem(i);
         }
+        
+        annee.setSelectedIndex(2015 - 1900);
+        
+        annee.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                setCalendar();
+                centre.updateUI();
+            }
+        });
         
         nord.add(mois);
         nord.add(annee);
         
-        centre.setLayout(new GridLayout(7, 7));
-        
-        centre.add(new JLabel("Lundi"));
-        centre.add(new JLabel("Mardi"));
-        centre.add(new JLabel("Mercredi"));
-        centre.add(new JLabel("Jeudi"));
-        centre.add(new JLabel("Vendredi"));
-        centre.add(new JLabel("Samedi"));
-        centre.add(new JLabel("Dimanche")); 
-        
-        int m = mois.getSelectedIndex() + 1;
-        int d = 1;
-        int y = annee.getSelectedIndex() + 1900;
-        
-        int D = getDayOfDate(m, d, y);
-        
-        System.out.println("Mois : " + m + "\nAnnée : " + y + "\nJour : " + D);
-        
-        int nb = 0;
-        
-        for (int i = 1; i < D; i++){
-            centre.add(new JLabel(Integer.toString(i)));
-            nb++;
-        }
-        
-        for (int i = 1; i <= 31; i++){
-            centre.add(new JLabel(Integer.toString(i)));
-            nb++;
-        }
-        
-        int i = 1;
-        while(nb < 42){
-            centre.add(new JLabel(Integer.toString(i)));            
-            i++;
-            nb++;
-        }
+        setCalendar();
         
         this.setLayout(new BorderLayout());
         this.add(nord, BorderLayout.NORTH);
-        this.add(centre, BorderLayout.CENTER);
-        
-        
+        this.add(centre, BorderLayout.CENTER);        
     }
     
     /**
@@ -161,5 +142,60 @@ public class PlanningUI extends JPanel {
             D = (((23*m)/9) + d + 4 + y + (z/4) - (z/100) + (z/400)) % 7;
         }
         return D;
+    }
+    
+    public void setCalendar(){
+        centre.removeAll();
+        
+        centre.add(new JLabel("Lundi"));
+        centre.add(new JLabel("Mardi"));
+        centre.add(new JLabel("Mercredi"));
+        centre.add(new JLabel("Jeudi"));
+        centre.add(new JLabel("Vendredi"));
+        centre.add(new JLabel("Samedi"));
+        centre.add(new JLabel("Dimanche")); 
+        
+        int m = mois.getSelectedIndex() + 1;
+        int d = 1;
+        int y = annee.getSelectedIndex() + 1900;
+        
+        int D = getDayOfDate(m, d, y);
+        
+        System.out.println("Mois : " + m + "\nAnnée : " + y + "\nJour : " + D);
+        
+        int nb = 0;
+        jours = new ArrayList<>();
+        
+        for (int i = 1; i < D; i++){
+            jours.add(new JLabel(Integer.toString(i+32-D)));
+            centre.add(jours.get(nb));
+            nb++;
+        }
+        
+        for (int i = 1; i <= 31; i++){
+            jours.add(new JLabel(Integer.toString(i)));
+            centre.add(jours.get(nb));
+            nb++;
+        }
+        
+        if (nb > 35){
+            int i = 1;
+            while(nb < 42){
+                jours.add(new JLabel(Integer.toString(i)));
+                centre.add(jours.get(nb));
+                i++;
+                nb++;
+            }
+            centre.setLayout(new GridLayout(7, 7));
+        } else {
+            int i = 1;
+            while(nb < 35){
+                jours.add(new JLabel(Integer.toString(i)));
+                centre.add(jours.get(nb));
+                i++;
+                nb++;
+            }
+            centre.setLayout(new GridLayout(6, 7));   
+        }
     }
 }
